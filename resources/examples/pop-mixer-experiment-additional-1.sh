428e66997bf126da03ce3647b0af351ffbc05fa9
JJ Merelo <jjmerelo@gmail.com>
2018-11-12 07:24:29 +0100

    Fixes experiment

--- resources/examples/pop-mixer-experiment-additional-1.sh
+++ resources/examples/pop-mixer-experiment-additional-1.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-gap='8 16 32 64'
+gap='16 64'
 for g in $gap
 do
     echo $g
-    ./population-mixer-old.p6 --generations=$g --population-size
+    ./population-mixer-old.p6 --generations=$g --population-size --repetitions=100
 done
