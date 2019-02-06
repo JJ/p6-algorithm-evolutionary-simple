d852c906ce1fecc9aa456f8b58459b85a13b868b
JJ Merelo <jjmerelo@gmail.com>
2019-02-06 19:05:28 +0100

    Correct script

--- resources/examples/pop-mixer-freq-gecco-threads-ap.sh
+++ resources/examples/pop-mixer-freq-gecco-threads-ap.sh
@@ -6,6 +6,6 @@ do
     echo $g
     for i in {1..15}
     do
-        perl6 -I../../lib population-mixer-freqs.p6 --threads=$g
+        perl6 -I../../lib population-mixer-freqs-ap.p6 --threads=$g
     done
 done
