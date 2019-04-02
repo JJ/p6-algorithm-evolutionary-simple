7c5b8a36e59447ff35b2a93120932cd702d69c4d
JJ Merelo <jjmerelo@gmail.com>
2019-04-02 06:15:12 +0200

    Better name for vars

--- resources/examples/leading-ones-scaling.sh
+++ resources/examples/leading-ones-scaling.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-gap='2 4 6 8 10'
-for g in $gap
+threads='2 4 6 8 10'
+for t in $threads
 do
-    echo $g
+    echo $t
     for i in {1..15}
     do
-        perl6 -I../../lib population-mixer-freqs-ap-lo.p6 --threads=$g
+        perl6 -I../../lib population-mixer-freqs-ap-lo.p6 --threads=$t
     done
 done
